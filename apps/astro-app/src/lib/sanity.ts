a4dfdca5b74ea80869b62a7cda384e29cdc06d0d
Andrii <andriisharapatov@gmail.com>
2026-02-17 13:22:46 +0200

    fix: update sanity client configuration to use process.env for projectId and dataset

--- apps/astro-app/src/lib/sanity.ts
+++ apps/astro-app/src/lib/sanity.ts
@@ -2,8 +2,8 @@ import { createClient } from "@sanity/client";
 import { createImageUrlBuilder } from "@sanity/image-url";
 
 export const sanityClient = createClient({
-  projectId: import.meta.env.SANITY_PROJECT_ID,
-  dataset: import.meta.env.SANITY_DATASET,
+  projectId: process.env.SANITY_PROJECT_ID,
+  dataset: process.env.SANITY_DATASET,
   apiVersion: "2024-01-01",
   useCdn: true,
 });
