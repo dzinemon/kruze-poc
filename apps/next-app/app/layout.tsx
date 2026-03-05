97efd8e26cceae956682fca5504b2c29ee35eaea
Andrii <andriisharapatov@gmail.com>
2026-03-05 15:16:14 +0200

    Conditionally render SanityLive component based on draft mode

--- apps/next-app/app/layout.tsx
+++ apps/next-app/app/layout.tsx
@@ -56,7 +56,7 @@ export default async function RootLayout({
           </div>
         </footer>
 
-        <SanityLive />
+        {isDraftMode && <SanityLive />}
         {isDraftMode && <VisualEditing />}
       </body>
     </html>
